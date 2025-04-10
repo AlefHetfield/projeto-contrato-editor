36e5cb3d7af717c7d3a6ed3c1f667ea799f96307
AlefHetfield <alefledz@gmail.com>
2025-04-09 22:16:49 -0300

    Atualiza a lógica de cadastro e exibição de participantes no contrato

--- app2.py
+++ app2.py
@@ -7,6 +7,8 @@ from classe_pessoa import Pessoa
 doc = Document("contrato_teste.docx")
 
 def main(): # Função principal que inicia o programa
+    comprador = ''
+    vendedor = ''
     tela_limpa()
     while True:
         tela_limpa()
@@ -16,40 +18,53 @@ def main(): # Função principal que inicia o programa
         print('[4] Converter para PDF')
         print('[5] Sair\n')
         
-        opcao_escolhida = int(input('Digite a opção desejada: '))
+        opcao_escolhida = input('Digite a opção desejada: ')
         
-        if opcao_escolhida == 1:
+        if opcao_escolhida == '1':
             while True:
                 tela_limpa()
                 print('CADASTRO DE PARTICIPANTES\n')
                 print('[1] Cadastrar Vendedores')
                 print('[2] Cadastrar Compradores')
                 print('[3] Voltar ao Menu Principal\n')
-                opcao_cadastro = int(input('Quem gostaria de cadastrar: '))
+                opcao_cadastro = input('Quem gostaria de cadastrar: ')
                 
-                if opcao_cadastro == 1:
+                if opcao_cadastro == '1':
                     tela_limpa()
                     vendedor = Pessoa("Vendedor")
                     vendedor.coletar_dados()
                     print('\nVENDEDOR CADASTRADO COM SUCESSO!\n')
                     input('Digite [ENTER] para voltar: ')
                 
-                elif opcao_cadastro == 2:
+                elif opcao_cadastro == '2':
                     tela_limpa()
                     comprador = Pessoa("Comprador")
                     comprador.coletar_dados()
                     print('\nCOMPRADOR CADASTRADO COM SUCESSO!\n')
                     input('Digite [ENTER] para voltar: ')
 
-                elif opcao_cadastro == 3:
-                    break
-                
+                elif opcao_cadastro == '3':
+                    break    
             continue
 
-        if opcao_escolhida == 2:
+        if opcao_escolhida == '2':
             tela_limpa()
-            print(comprador, vendedor)
+            print(comprador)
+            print('-' * 30)
+            print(vendedor)
             input('Digite [ENTER] para voltar: ')
+
+        if opcao_escolhida == '3':
+            nome_arquivo = input('Digite o nome do arquivo que deseja salvar: ')
+            doc.save(f'{nome_arquivo}.docx')
+
+
+
+        if opcao_escolhida == '5':
+            os.system('cls')
+            print('TCHAU. ATÉ A PRÓXIMA!')
+            print('\n© Motive Consultoria Imobiliária LTDA.\n')
+            break
 
 
 
@@ -64,10 +79,6 @@ def exibir_nome_programa(): # Exibe o nome do programa na tela
 def tela_limpa(): #Limpa a tela e exibe o nome do programa
     os.system('cls')
     exibir_nome_programa()
-
-
-
-
 
 
 
