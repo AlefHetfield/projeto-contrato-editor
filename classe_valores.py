30de9d2fe781e916602dfc8846726b175e7a919e
Adriano da Silva <arias_ari@hotmail.com>
2025-04-25 16:47:26 -0300

    Adicionando função para coletar nome do 'Banco'

--- classe_valores.py
+++ classe_valores.py
@@ -1,6 +1,7 @@
 class Valores:
     def __init__(self, tipo):
         self.tipo = tipo # Valores
+        self.banco = ''
         self.valor_do_imovel = ''
         self.sinal = ''
         self.fgts = ''
@@ -8,7 +9,8 @@ class Valores:
         self.financiamento = '' 
 
     def coletar_dados(self):
-        print('CADASTRO DOS VALORES DA NEGOCIAÇÃO\n')
+        print('CADASTRO DOS DADOS DA NEGOCIAÇÃO\n')
+        self.banco = input('Banco a financiar: ')
         self.valor_do_imovel = input('Valor do imóvel: R$ ')    
         self.sinal = input('Valor do sinal. Caso não se aplique, deixe em branco: R$ ')
         self.fgts = input('Valor do FGTS. Caso não se aplique, deixe em branco: R$ ')
@@ -16,44 +18,50 @@ class Valores:
         self.financiamento = input('Valor do financiamento: R$ ')
 
     def __str__(self):
-        return f'{self.tipo.upper()}\nValor do Imóvel: R$ {self.valor_do_imovel}\nSinal: R$ {self.sinal}\nFGTS: R$ {self.fgts}\nRecursos Próprios: R$ {self.recursos_proprios}\nFinanciamento: R$ {self.financiamento}'
+        return f'{self.tipo.upper()}\nBanco: {self.banco}\nValor do Imóvel: R$ {self.valor_do_imovel}\nSinal: R$ {self.sinal}\nFGTS: R$ {self.fgts}\nRecursos Próprios: R$ {self.recursos_proprios}\nFinanciamento: R$ {self.financiamento}'
     
     def editar_dados(self):
         while True:
             print(f'\nEditar dados dos {self.tipo}')
-            print('[1] Valor do Imóvel')
-            print('[2] Valor do Sinal')
-            print('[3] Valor do FGTS')
-            print('[4] Valor em Recursos Próprios')
-            print('[5] Valor do Financiamento')
-            print('[6] Voltar')
+            print('[1] Nome do Banco')
+            print('[2] Valor do Imóvel')
+            print('[3] Valor do Sinal')
+            print('[4] Valor do FGTS')
+            print('[5] Valor em Recursos Próprios')
+            print('[6] Valor do Financiamento')
+            print('[7] Voltar')
             opcao = input('\nEscolha a opção que deseja alterar: ')
             if opcao == '1':
+                self.banco = input('Digite o novo nome do banco: ')
+                print(f'Banco alterado para: {self.banco}')
+                input('Digite [ENTER] para voltar.')
+                break            
+            elif opcao == '2':
                 self.valor_do_imovel = input('Digite o novo valor: R$ ')
                 print(f'Valor do Imóvel alterado para: R$ {self.valor_do_imovel}')
                 input('Digite [ENTER] para voltar.')
                 break
-            elif opcao == '2':
+            elif opcao == '3':
                 self.sinal = input('Digite o novo valor: R$ ')
                 print(f'Valor do Sinal alterado para: R$ {self.sinal}')
                 input('Digite [ENTER] para voltar.')
                 break
-            elif opcao == '3':
+            elif opcao == '4':
                 self.fgts = input('Digite o novo valor: R$ ')
                 print(f'Valor de FGTS alterado para: R$ {self.fgts}')
                 input('Digite [ENTER] para voltar.')
                 break
-            elif opcao == '4':
+            elif opcao == '5':
                 self.recursos_proprios = input('Digite o novo valor: R$ ')
                 print(f'Valor de Recursos Próprios alterado para: R$ {self.recursos_proprios}')
                 input('Digite [ENTER] para voltar.')
                 break
-            elif opcao == '5':
+            elif opcao == '6':
                 self.financiamento = input('Digite o novo valor: R$ ')
                 print(f'Valor de Financiamento alterado para: R$ {self.financiamento}')
                 input('Digite [ENTER] para voltar.')
                 break
-            elif opcao == '6':
+            elif opcao == '7':
                 break
             else:
                 print('Opção inválida. Digite novamente.')
